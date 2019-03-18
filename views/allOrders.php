1137e5ec99eaae8965ffbebf890a7a0229ec676d
Andrey <voprosi-1@yandex.ru>
2019-03-18 16:45:39 +0300

    добавил Bootstrap карточку

--- views/allOrders.php
+++ views/allOrders.php
@@ -2,14 +2,20 @@
 /** @var \app\model\Orders $order */
 ?>
 
-<h2>Наименование:<?=$order->product?></h2>
-<p>Артикул:<?=$order->product_id?></p>
-<p>Цена:<?=$order->price?></p>
-<p>Количество:<?=$order->quantity?></p>
-<p>Имя:<?=$order->f_name?></p>
-<p>Фамилия:<?=$order->s_name?></p>
-<p>Адрес:<?=$order->address?></p>
-<p>Индекс:<?=$order->post_index?></p>
-<p>Электронная почта:<?=$order->email?></p>
-<p>Дата заказа:<?=$order->date?></p>
-<p>Время заказа:<?=$order->time?></p>
+<div class="card" style="width: 18rem;">
+  <img src="https://img.povar.ru/main/8e/b1/d4/01/picca_myasnaya-387507.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h2 class="card-title"><?=$order->product?></h2>
+    <p class="card-text">Артикул:<?=$order->product_id?></p>
+<p class="card-text">Цена:<?=$order->price?></p>
+<p class="card-text">Количество:<?=$order->quantity?></p>
+<p class="card-text">Имя:<?=$order->f_name?></p>
+<p class="card-text">Фамилия:<?=$order->s_name?></p>
+<p class="card-text">Адрес:<?=$order->address?></p>
+<p class="card-text">Индекс:<?=$order->post_index?></p>
+<p class="card-text">Электронная почта:<?=$order->email?></p>
+<p class="card-text">Дата заказа:<?=$order->date?></p>
+<p class="card-text">Время заказа:<?=$order->time?></p>
+    <a href="#" class="btn btn-primary">Купить</a>
+  </div>
+</div>
